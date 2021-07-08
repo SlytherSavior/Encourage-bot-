05e58a411a7a902857af488c2d0cf50fe61354d9
Slyther coder <85357750+ShrijanPoudel@users.noreply.github.com>
2021-07-08 16:25:11 +0000

    many msgs

--- index.js
+++ index.js
@@ -22,7 +22,27 @@ client.on("message", msg => {
     getQuote().then(quote => msg.channel.send(quote))
   }
 })
-
+client.on("message", msg => {
+  if(msg.authoer.bot) return
+  if(msg.content === "$help") {
+    msg.reply("i am and encourager and conversator bot , do $inspire to get quotes , my prefix is $")
+  }
+})
+client.on("message", msg => {
+  if(msg.content === "$invite"){
+    msg.reply("https://discord.com/api/oauth2/authorize?client_id=862348917098283058&permissions=8&scope=bot")
+  }
+})
+client.on("message", msg => {
+  if(msg.content === "$yt"){
+    msg.reply("https://www.youtube.com/channel/UCKGhP5D5l5UriOde9tGR8qAt")
+  }
+})
+client.on("message", msg => {
+  if(msg.content === "$i am sad"){
+    msg.reply("https://www.youtube.com/watch?v=BnYSSMbZdaY&t=1st")
+  }
+})
 client.login(process.env.TOKEN)
 const mySecret = process.env['TOKEN']
 
