3a3464d04e0f75a8e45496230d8c7d2b719cb133
Slyther coder <85357750+ShrijanPoudel@users.noreply.github.com>
2021-07-08 16:04:33 +0000

    NEw quote command

--- index.js
+++ index.js
@@ -1,45 +1,28 @@
 const Discord = require("discord.js")
+const fetch = require ("node-fetch")
 const client = new Discord.Client()
+
+function getQuote()  {
+  return fetch("https://zenquotes.io/api/random")
+   .then(res =>{
+      return res.json()
+   })
+   .then(data => {
+     return data[0]["q"] + "-" + ["a"]
+   })
+}
 
 client.on("ready", () => {
   console.log('Logged in as ${client.user.tag}!')
 })
 
 client.on("message", msg => {
-  if (msg.content === "ping") {
-    msg.reply("pong")
+  if(msg.author.bot) return
+  if (msg.content === "$inspire") {
+    getQuote().then(quote => msg.channel.send(quote))
   }
 })
-client.on("message", msg => {
-  if (msg.content === "hello") {
-    msg.reply("hello how are u doin")
-  }
-})
-client.on("message", msg=> {
-  if (msg.content === "k xa yr") {
-    msg.reply("thik xa ni , tero k xa")
-  }
-})
-client.on("message", msg=> {
-  if (msg.content === "i am sad"){
-    msg.reply("why are you sad? please listen to this - https://www.youtube.com/watch?v=BnYSSMbZdaY")
-  }
-})
-client.on("message", msg=> {
-  if (msg.content === "sahi ho"){
-    msg.reply("nepali ho ni sathi")
-  }
-})
-client.on("message", msg => {
-  if (msg.content === "/invite"){
-    msg.reply("https://discord.com/api/oauth2/authorize?client_id=862348917098283058&permissions=8&scope=bot")
-  }
-})
-client.on("message", msg => {
-  if (msg.content === "/YT"){
-    msg.reply("GO fast and sub to this legend https://www.youtube.com/channel/UCKGhP5D5l5UriOde9tGR8qA")
-  }
-})
+
 client.login(process.env.TOKEN)
 const mySecret = process.env['TOKEN']
 
