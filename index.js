afe6751b6abefa3acf75d3bf7271a506b1c463dd
Slyther coder <85357750+ShrijanPoudel@users.noreply.github.com>
2021-07-08 16:07:50 +0000

    data

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ const client = new Discord.Client()
 
 function getQuote()  {
   return fetch("https://zenquotes.io/api/random")
-   .then(res =>{
+   .then(res => {
       return res.json()
    })
    .then(data => {
-     return data[0]["q"] + "-" + ["a"]
+     return data[0]["q"] + "-" + data[0]["a"]
    })
 }
 
