462378ab250642a3bbb8937f3418cb7df6c402f2
Slyther coder <85357750+ShrijanPoudel@users.noreply.github.com>
2021-07-09 10:13:53 +0000

    jyf

--- index.js
+++ index.js
@@ -15,10 +15,6 @@ function getQuote()  {
 client.on("ready", () => {
   console.log('Logged in as ${client.user.tag}!')
 })
-bot.on('ready', () => {
-  console.log('Bot is ready!');
-  bot.user.setActivity('Prefix is: .', { type: "WATCHING" }).catch(console.error)
-})
 client.on("message", msg => {
   if(msg.author.bot) return
   if (msg.content === "$inspire") {
